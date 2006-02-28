67cb5f75b4e7baacd003e0e303124cf9f3eef053
tailor <martin@piware.de>
2006-02-28 18:40:23 +0100

    pmount: allow mounting of mmc cards

--- src/policy.c
+++ src/policy.c
@@ -366,7 +366,7 @@ int
 device_removable( const char* device )
 {
     struct sysfs_device *dev;
-    static char* hotplug_buses[] = { "usb", "ieee1394", NULL };
+    static char* hotplug_buses[] = { "usb", "ieee1394", "mmc", NULL };
     int removable;
     char blockdevpath[PATH_MAX];
 
